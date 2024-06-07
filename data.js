cc0aa494d8cd2fb3ba048c01545f946ae9fd8d8a
Saliha Kotan <salihakotan77@gmail.com>
2024-06-08 01:56:26 +0300

    subscription for createUser added

--- data.js
+++ data.js
@@ -1,22 +1,18 @@
-const users = [
+export const users = [
     { id: "1", fullname: "Buket Soyhan" },
     { id: "2", fullname: "Asli Demirci" },
   ];
   
-  const posts = [
+  export const posts = [
     { id: "1", title: "Buketin gönderisi", user_id: "1" },
     { id: "2", title: "Buketin 2. gönderisi", user_id: "1" },
     { id: "3", title: "Aslinin gönderisi", user_id: "2" },
   ];
   
-  const comments = [
+  export const comments = [
     { id: "1", text: "buketin yorumu", post_id: "1", user_id: "1" },
     { id: "2", text: "aslının yorumu", post_id: "1", user_id: "2"},
     { id: "3", text: "buketin 2.yorumu", post_id: "2", user_id: "1" },
   ];
   
-  module.exports = {
-    users,
-    posts,
-    comments,
-  };
+  
--- index.js
+++ index.js
@@ -1,277 +1,331 @@
-const { ApolloServer, gql } = require("apollo-server");
-const {
-  ApolloServerPluginLandingPageGraphQLPlayground,
-} = require("apollo-server-core");
-const { comments, users, posts } = require("./data");
+// import { ApolloServer, gql } from "apollo-server";
+// import {
+//   ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core";
 
-const { nanoid } = require("nanoid");
+import { createServer } from 'node:http'
+import { createSchema, createYoga } from 'graphql-yoga'
+// import { createPubSub } from 'graphql-yoga';
 
-const typeDefs = gql`
-  type User {
-    id: ID!
-    fullname: String!
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
+import {PubSub} from "graphql-subscriptions"
 
-  input CreateUserInput {
-    fullname: String!
-  }
-
-  input UpdateUserInput {
-    fullname: String
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    user_id: ID!
-    user: User!
-    comments: [Comment!]!
-  }
-
-  input CreatePostInput {
-    title: String!
-    user_id: ID!
-  }
-
-  input UpdatePostInput{
-    title:String
-    user_id:ID
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    post_id: ID!
-    user_id: ID!
-    user: User!
-    post: Post!
-  }
-
-  input CreateCommentInput {
-    text: String!
-    post_id: ID!
-    user_id: ID!
-  }
-
-  input UpdateCommentInput{
-    text:String
-    post_id:ID
-    user_id:ID
-  }
+import { comments, users, posts } from "./data.js";
 
 
-  type DeleteAllOutput{
-    count: Int!
-  }
+import { nanoid } from "nanoid";
+import { subscribe } from 'node:diagnostics_channel';
 
-  type Query {
-    users: [User!]!
-    user(id: ID!): User!
+// const pubsub = createPubSub()
+const pubsub = new PubSub()
 
-    posts: [Post!]!
-    post(id: ID!): Post!
-
-    comments: [Comment!]!
-    comment(id: ID!): Comment!
-  }
-
-  type Mutation {
-    createUser(data: CreateUserInput!): User!
-    updateUser(id:ID!,data: UpdateUserInput!): User!
-    deleteUser(id:ID!):User!
-    deleteAllUsers: DeleteAllOutput!
-
-    createPost(data: CreatePostInput!): Post!
-    updatePost(id:ID!,data:UpdatePostInput!): Post!
-    deletePost(id:ID!):Post!
-    deleteAllPosts: DeleteAllOutput!
-
-    createComment(data: CreateCommentInput!): Comment!
-    updateComment(id:ID!, data: UpdateCommentInput!): Comment!
-    deleteComment(id:ID!): Comment!
-    deleteAllComments: DeleteAllOutput!
+const yoga = createYoga({
+  schema: createSchema({
     
+    typeDefs: /* GraphQL */ `
+type User {
+  id: ID!
+  fullname: String!
+  posts: [Post!]!
+  comments: [Comment!]!
+}
+
+input CreateUserInput {
+  fullname: String!
+}
+
+input UpdateUserInput {
+  fullname: String
+}
+
+type Post {
+  id: ID!
+  title: String!
+  user_id: ID!
+  user: User!
+  comments: [Comment!]!
+}
+
+input CreatePostInput {
+  title: String!
+  user_id: ID!
+}
+
+input UpdatePostInput{
+  title:String
+  user_id:ID
+}
+
+type Comment {
+  id: ID!
+  text: String!
+  post_id: ID!
+  user_id: ID!
+  user: User!
+  post: Post!
+}
+
+input CreateCommentInput {
+  text: String!
+  post_id: ID!
+  user_id: ID!
+}
+
+input UpdateCommentInput{
+  text:String
+  post_id:ID
+  user_id:ID
+}
+
+
+type DeleteAllOutput{
+  count: Int!
+}
+
+type Query {
+  users: [User!]!
+  user(id: ID!): User!
+
+  posts: [Post!]!
+  post(id: ID!): Post!
+
+  comments: [Comment!]!
+  comment(id: ID!): Comment!
+}
+
+type Mutation {
+  createUser(data: CreateUserInput!): User!
+  updateUser(id:ID!,data: UpdateUserInput!): User!
+  deleteUser(id:ID!):User!
+  deleteAllUsers: DeleteAllOutput!
+
+  createPost(data: CreatePostInput!): Post!
+  updatePost(id:ID!,data:UpdatePostInput!): Post!
+  deletePost(id:ID!):Post!
+  deleteAllPosts: DeleteAllOutput!
+
+  createComment(data: CreateCommentInput!): Comment!
+  updateComment(id:ID!, data: UpdateCommentInput!): Comment!
+  deleteComment(id:ID!): Comment!
+  deleteAllComments: DeleteAllOutput!
+  
+}
+
+  type Subscription{
+    countdown:Int!
+    userCreated: User!
   }
-`;
-const resolvers = {
-  Mutation: {
-    createUser: (parent, { data }) => {
-      const user = {
-        id: nanoid(),
-        // fullname:data.fullname,
-        //other using
-        ...data, //get all datas
-      };
 
-      users.push(user);
-      return user;
+
+`,
+ resolvers:{
+
+  Subscription:{
+    countdown: {
+      subscribe:(_,__)=> pubsub.asyncIterator("countdown")
+
+      // async function* (_,__ ) {
+      
+      //    for (let i = 0; i <= 10; i++) {
+      //         await new Promise(resolve => setTimeout(resolve, 1000))
+      //         yield { countdown: i }
+      //       }
+        
+      // },
+    
     },
-
-    updateUser: (parent, {id,data}) => {
-        const user_index = users.findIndex((user)=> user.id ===id)
-
-        if(user_index === -1){
-            throw new Error("User not found")
-        }
-
-        const updated_user = users[user_index] = {
-            ...users[user_index],
-            ...data
-        }
-
-        return updated_user
-
-    },
-
-    deleteUser: (parent, {id}) => {
-        const user_index = users.findIndex((user)=> user.id === id)
-
-        if(user_index === -1) {
-            throw new Error("User not found")
-        }
-
-        const deleted_user = users[user_index]
-        users.splice(user_index,1)
-        return deleted_user
-    },
-
-    deleteAllUsers: ()=> {
-        const length = users.length
-        users.splice(0,length)
-        return {
-            count: length
-        }
-    },
-
-
-    createPost: (parent, { data }) => {
-      const post = {
-        id: nanoid(),
-        ...data,
-      };
-
-      posts.push(post);
-
-      return post;
-    },
-
-    updatePost: (parent, {id,data}) => {
-        const post_index = posts.findIndex((post)=> post.id ===id)
-
-        if(post_index ===-1){
-            throw new Error("Post not found")
-        }
-
-        const updated_post = posts[post_index] = {
-            ...posts[post_index],
-            ...data
-        }
-
-        return updated_post
-    },
-
-    deletePost: (parent, {id}) => {
-        const post_index = posts.findIndex((post)=> post.id === id) 
-        if(post_index === -1) {
-            throw new Error("Post not found")
-        }
-
-        const deleted_post = posts[post_index]
-        posts.splice(post_index,1)
-        return deleted_post
-    },
-
-    deleteAllPosts: ()=> {
-        const length = posts.length
-        posts.splice(0,length)
-        return {count:length}
-    },
-
-    createComment: (parent, { data }) => {
-      const comment = {
-        id: nanoid(),
-        // text, //short using
-        // user_id,//short using
-        // post_id//short using
-        ...data,
-      };
-
-      comments.push(comment);
-      return comment;
-    },
-
-    updateComment: (parent, {id,data}) => {
-        const comment_index = comments.findIndex((comment)=> comment.id === id)
-
-        if(comment_index === -1){
-            throw new Error("Comment not found")
-        }
-
-        const updated_comment = comments[comment_index] = {
-            ...comments[comment_index],
-            ...data
-        }
-
-        return updated_comment
-    },
-
-    deleteComment: (parent, {id})=> {
-        const comment_index = comments.findIndex((comment)=> comment.id === id)
-        if(comment_index === -1) {
-            throw new Error("Comment not found")
-        }
-        const deleted_comment = comments[comment_index]
-        comments.splice(comment_index,1)
-        return deleted_comment
-    },
-    deleteAllComments: ()=> {
-        const length = comments.length
-        comments.splice(0,length)
-        return {count:length}
+    userCreated:{
+      subscribe: (_,__)=> pubsub.asyncIterator("userCreated")
     }
   },
 
-  Query: {
-    users: () => users,
-    user: (parent, args) => users.find((user) => user.id === args.id),
+Mutation: {
+  createUser: (parent, { data }) => {
+    const user = {
+      id: nanoid(),
+      // fullname:data.fullname,
+      //other using
+      ...data, //get all datas
+    };
 
-    posts: () => posts,
-    post: (parent, args) => posts.find((post) => post.id === args.id),
+    users.push(user);
 
-    comments: () => comments,
-    comment: (parent, args) =>
-      comments.find((comment) => comment.id === args.id),
+    pubsub.publish("userCreated", {userCreated: user}).then(console.log("user created"))
+
+
+    return user;
   },
 
-  User: {
-    posts: (parent, args) => posts.filter((post) => post.user_id === parent.id),
-    comments: (parent) =>
-      comments.filter((comment) => comment.user_id === parent.id),
+  updateUser: (parent, {id,data}) => {
+      const user_index = users.findIndex((user)=> user.id ===id)
+
+      if(user_index === -1){
+          throw new Error("User not found")
+      }
+
+      const updated_user = users[user_index] = {
+          ...users[user_index],
+          ...data
+      }
+
+      return updated_user
+
   },
 
-  Post: {
-    user: (parent, args) => users.find((user) => user.id === parent.user_id),
-    comments: (parent, args) =>
-      comments.filter((comment) => comment.post_id === parent.id),
-  },
-  Comment: {
-    user: (parent, args) => users.find((user) => user.id === parent.user_id),
-    post: (parent, args) => posts.find((post) => post.id === parent.post_id),
-  },
-};
+  deleteUser: (parent, {id}) => {
+      const user_index = users.findIndex((user)=> user.id === id)
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground({
-      //options
-    }),
-  ],
-});
+      if(user_index === -1) {
+          throw new Error("User not found")
+      }
 
-server.listen().then(({ url }) => {
-  console.log(`Server is up at ${url}`);
-});
+      const deleted_user = users[user_index]
+      users.splice(user_index,1)
+      return deleted_user
+  },
+
+  deleteAllUsers: ()=> {
+      const length = users.length
+      users.splice(0,length)
+      return {
+          count: length
+      }
+  },
+
+
+  createPost: (parent, { data }) => {
+    const post = {
+      id: nanoid(),
+      ...data,
+    };
+
+    posts.push(post);
+
+    return post;
+  },
+
+  updatePost: (parent, {id,data}) => {
+      const post_index = posts.findIndex((post)=> post.id ===id)
+
+      if(post_index ===-1){
+          throw new Error("Post not found")
+      }
+
+      const updated_post = posts[post_index] = {
+          ...posts[post_index],
+          ...data
+      }
+
+      return updated_post
+  },
+
+  deletePost: (parent, {id}) => {
+      const post_index = posts.findIndex((post)=> post.id === id) 
+      if(post_index === -1) {
+          throw new Error("Post not found")
+      }
+
+      const deleted_post = posts[post_index]
+      posts.splice(post_index,1)
+      return deleted_post
+  },
+
+  deleteAllPosts: ()=> {
+      const length = posts.length
+      posts.splice(0,length)
+      return {count:length}
+  },
+
+  createComment: (parent, { data }) => {
+    const comment = {
+      id: nanoid(),
+      // text, //short using
+      // user_id,//short using
+      // post_id//short using
+      ...data,
+    };
+
+    comments.push(comment);
+    return comment;
+  },
+
+  updateComment: (parent, {id,data}) => {
+      const comment_index = comments.findIndex((comment)=> comment.id === id)
+
+      if(comment_index === -1){
+          throw new Error("Comment not found")
+      }
+
+      const updated_comment = comments[comment_index] = {
+          ...comments[comment_index],
+          ...data
+      }
+
+      return updated_comment
+  },
+
+  deleteComment: (parent, {id})=> {
+      const comment_index = comments.findIndex((comment)=> comment.id === id)
+      if(comment_index === -1) {
+          throw new Error("Comment not found")
+      }
+      const deleted_comment = comments[comment_index]
+      comments.splice(comment_index,1)
+      return deleted_comment
+  },
+  deleteAllComments: ()=> {
+      const length = comments.length
+      comments.splice(0,length)
+      return {count:length}
+  }
+},
+
+Query: {
+  users: () => users,
+  user: (parent, args) => users.find((user) => user.id === args.id),
+
+  posts: () => posts,
+  post: (parent, args) => posts.find((post) => post.id === args.id),
+
+  comments: () => comments,
+  comment: (parent, args) =>
+    comments.find((comment) => comment.id === args.id),
+},
+
+User: {
+  posts: (parent, args) => posts.filter((post) => post.user_id === parent.id),
+  comments: (parent) =>
+    comments.filter((comment) => comment.user_id === parent.id),
+},
+
+Post: {
+  user: (parent, args) => users.find((user) => user.id === parent.user_id),
+  comments: (parent, args) =>
+    comments.filter((comment) => comment.post_id === parent.id),
+},
+Comment: {
+  user: (parent, args) => users.find((user) => user.id === parent.user_id),
+  post: (parent, args) => posts.find((post) => post.id === parent.post_id),
+},
+}
+  })
+})
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   plugins: [
+//     ApolloServerPluginLandingPageGraphQLPlayground({
+//       //options
+//     }),
+//   ],
+// });
+
+// server.listen().then(({ url }) => {
+//   console.log(`Server is up at ${url}`);
+// });
+
+
+
+const server = createServer(yoga)
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
